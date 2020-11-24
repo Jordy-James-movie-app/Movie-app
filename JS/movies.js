8b22d672c2f3f5d4ad4ad9a6a86c7a90c7f7c147
jordy muniz <jordy.muniz96@gmail.com>
2020-11-24 14:17:23 -0600

    update

---
+++ JS/movies.js
@@ -0,0 +1,21 @@
+"use strict"
+//url for glitch movie API
+const url = "https://ethereal-generated-marshmallow.glitch.me/movies"
+
+ // @returns {Promise<void>}
+
+const getMovies = () => fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(console.error);
+
+// getMovies().then(console.log);
+
+getMovies().then(movies => {
+    let html = '<ul>';
+    for (let movie of movies) {
+        html += `<li>${movie.name}</li>`
+    }
+    html += '</ul>';
+    document.write(html);
+});
